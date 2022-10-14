b49a9889110180bd52a2377e3c21c4366921d799
Joao C de Franca <joao.franca@clix.com.br>
2022-10-14 11:10:56 -0300

    primeiro commit

---
+++ Joao.SOAP/WebService/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CepCorreiosSOAP;
+
+await consumirCepCorreios("59775000");
+
+async Task consumirCepCorreios(string cep)
+{
+    AtendeClienteClient client = new AtendeClienteClient();
+
+    consultaCEP consultaCEP = new consultaCEP(cep);
+    consultaCEPResponse consultaCEPResponse = await client.consultaCEPAsync(consultaCEP);
+
+    Console.WriteLine(consultaCEPResponse.@return.cidade);
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # joao.soap.cepcorreios
+
+Cep Correios SOAP Web Service
+
+URL Service: https://apps.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente?wsdl
+
